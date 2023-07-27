1fee0f8cee3c2a2c387fa760b4e6dd7925558c19
gywgithub <qingyi_w@outlook.com>
2023-07-27 11:22:30 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+    host: '0.0.0.0' // 这样配置在Docker中运行后可以在容器外部访问
+  },
   plugins: [
     vue(),
     vueJsx(),
